42f40e8604b1dfbe5a1b929db07592caa0b59ca0
Emil Berg <emil94berg@gmail.com>
2026-03-11 10:26:11 +0100

    first

--- fim.client/src/App.tsx
+++ fim.client/src/App.tsx
@@ -1,52 +1,24 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { Home } from './Home' 
 function App() {
-    const [count, setCount] = useState(0)
-    const [count2, setCount2] = useState(0)
+   
 
-    useEffect(() => {
-        console.log('Count has changed:', count)
-    }, [count])
+    
 
-    useEffect(() => {
-        console.log('First render, never to be seen again')
-    }, [])
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        </div>
-        <div className="card">
-            <button onClick={() => setCount2((count) => count + 1)}>
-                  count2 is {count2}
-            </button>
-            <p>
-                Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-        </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    return (
+        <BrowserRouter>
+            <Home/>
+            <Routes>
+                
+            </Routes>
+            <Route>
+            </Route>
+            <Link>
+            </Link>
+      </BrowserRouter>
+    
   )
 }
-//Uswstate useefect
+//Usestate useeffect
 export default App
