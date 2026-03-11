9489801f82c464c183c8878d74ddc005431d7dbc
Emil Berg <emil94berg@gmail.com>
2026-03-11 11:42:13 +0100

    changed package.Json

--- fim.client/src/App.tsx
+++ fim.client/src/App.tsx
@@ -1,24 +1,13 @@
-import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import { Home } from './Home' 
-function App() {
-   
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
 
-    
 
+export default function App() {
     return (
-        <BrowserRouter>
-            <Home/>
-            <Routes>
-                
-            </Routes>
-            <Route>
-            </Route>
-            <Link>
-            </Link>
-      </BrowserRouter>
-    
-  )
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    );
 }
 //Usestate useeffect
-export default App
+
