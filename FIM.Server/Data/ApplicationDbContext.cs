d4b8dd706c85001ee7f5b2c44aa797c015488493
Oscar Lejon <oscar.lejon@campusnykoping.se>
2026-03-13 13:43:11 +0100

    Changed to use localdb and supabase for auth only

---
+++ FIM.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using FIM.Server.Models;
+
+namespace FIM.Server.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Spool> Spools { get; set; }
+    public DbSet<Print> Prints { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Spool>()
+            .Property(s => s.SpoolCost)
+            .HasPrecision(18, 2);
+    }
+}
