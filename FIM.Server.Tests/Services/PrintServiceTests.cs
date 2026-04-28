9223740738310b4f1588570ba0947b8bd4c19a4c
Oscar Lejon <oscar.lejon@campusnykoping.se>
2026-04-28 10:48:13 +0200

    Added testfile

---
+++ FIM.Server.Tests/Services/PrintServiceTests.cs
@@ -0,0 +1,12 @@
+using FIM.Server.DTOs.PrintDtos;
+using FIM.Server.Models;
+using FIM.Server.Services;
+using FIM.Server.Tests.TestInfrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIM.Server.Tests.Services;
+
+public class PrintServiceTests
+{
+    
+}
