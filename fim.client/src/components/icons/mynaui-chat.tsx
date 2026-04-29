1998a4c827cd5ce0e4cc230e4b394b6e9193f0cb
Oscar Lejon <oscar.lejon@campusnykoping.se>
2026-04-29 10:56:24 +0200

    Add new layout and UI for posts and comments

---
+++ fim.client/src/components/icons/mynaui-chat.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+export function ChatIcon({
+  size = 24,
+  color = "currentColor",
+  strokeWidth = 2,
+  className,
+  ...props
+}: React.SVGProps<SVGSVGElement> & {
+  size?: number;
+  color?: string;
+  strokeWidth?: number;
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      {...props}
+    >
+      <path d="M12 21a9 9 0 1 0-9-9c0 1.44.338 2.8.94 4.007c.453.911-.177 2.14-.417 3.037a1.17 1.17 0 0 0 1.433 1.433c.897-.24 2.126-.87 3.037-.416A9 9 0 0 0 12 21"/>
+    </svg>
+  );
+}
