a6aa3278ae6010a61d154c9e5d67fe3737fe5c72
Emil Berg <emil94berg@gmail.com>
2026-04-30 10:30:12 +0200

    sidebar forum + icons

--- fim.client/src/components/SidebarMenu.tsx
+++ fim.client/src/components/SidebarMenu.tsx
@@ -5,6 +5,8 @@ import { HomeIcon } from "@/components/icons/mynaui-home"
 import { AddQueueIcon } from "@/components/icons/mynaui-add-queue"
 import { ActivitySquareIcon } from "@/components/icons/mynaui-activity-square"
 import { SquareDashedKanbanIcon } from "@/components/icons/mynaui-square-dashed-kanban"
+import { BookIcon } from "@/components/icons/mynaui-book"
+import { SmileSquareIcon } from "@/components/icons/mynaui-smile-square"
 
 const components: { title: string; href: string; icon?: React.ReactNode }[] = [
     {
@@ -33,9 +35,14 @@ const components: { title: string; href: string; icon?: React.ReactNode }[] = [
         icon: <ActivitySquareIcon className="w-5 h-5" ></ActivitySquareIcon>
     },
     {
+        title: "Forum",
+        href: "/forum",
+        icon: <BookIcon className="w-5 h-5" ></BookIcon>
+    },
+    {
         title: "Profile Page",
         href: "/profile",
-        icon: <HomeIcon className="w-5 h-5" ></HomeIcon>
+        icon: <SmileSquareIcon className="w-5 h-5" ></SmileSquareIcon>
     }
 ]
 
@@ -43,18 +50,6 @@ const components: { title: string; href: string; icon?: React.ReactNode }[] = [
 export default function SideBar() {
     const [open, setOpen] = useState(true);
     return (
-        //<div style={{ maxWidth: "200px", height: "100%" }}>
-        //    <Button className="bg-blue-500" onClick={() => setOpen(!open)}>☰</Button>
-        //    {open && (
-        //        <nav style={{ border: "1px solid black", margin: "5px", padding: "5px", backgroundColor: "beige" }}>
-        //            <ul>
-        //                <li>Home</li>
-        //                <li>About</li>
-        //                <li>Contact</li>
-        //            </ul>
-        //        </nav>
-        //    )}
-        //</div>
         <div className="flex">
             <div className={`flex flex-col bg-gray-100 h-screen transition-all duration-300 ${open ? "w-48" : "w-12"}`}>
                 <Button className="m-2 p-2 bg-blue-500 text-white" onClick={() => setOpen(!open)}>☰</Button>
